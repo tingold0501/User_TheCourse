1ad8b23ba6f73e77da362c37db771fa9f708618c
Phạm Huỳnh Tín <65479150+tingold0501@users.noreply.github.com>
2023-12-19 13:16:59 +0700

    ....

---
+++ thecourse_user/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import Header from '../component/Header'
+
+function Home() {
+    return (
+        <div>
+            <Header/>
+        </div>
+    )
+}
+
+export default Home
