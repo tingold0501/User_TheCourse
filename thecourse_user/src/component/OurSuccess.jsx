601d01c14d605248f8678b059bc1373ce5b43526
Phạm Huỳnh Tín <65479150+tingold0501@users.noreply.github.com>
2023-12-19 17:56:48 +0700

    ....

---
+++ thecourse_user/src/component/OurSuccess.jsx
@@ -0,0 +1,20 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+
+function OurSuccess() {
+    return (
+        <div className='w-full'>
+            <div className='w-full text-center mt-[5%]'>
+                <p className='text-5xl font-bold'>Our Success</p>
+                <p className='text-lg mt-5 '>Ornare id fames interdum porttitor nulla turpis etiam. Diam vitae sollicitudin at nec nam et pharetra gravida. Adipiscing a quis ultrices eu ornare tristique vel nisl orci.</p>
+            </div>
+            <div className='w-full'>
+                <div>
+                    
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default OurSuccess
