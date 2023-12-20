76e07a10b9809ba4ad90af4728a72853eb2d15fb
Phạm Huỳnh Tín <65479150+tingold0501@users.noreply.github.com>
2023-12-20 22:04:30 +0700

    ....

---
+++ thecourse_user/src/component/TOTC.jsx
@@ -0,0 +1,77 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+
+function TOTC() {
+    return (
+        <div>
+            {/* component */}
+            <section className="py-10 bg-w sm:py-16 lg:py-24 z-40 relative">
+                <div className="container mx-auto">
+                    <h2 className="text-3xl font-light text-center text-black sm:text-4xl lg:text-5xl">
+                        What is <span className="block w-full font-light text-transparent bg-clip-text bg-gradient-to-r from-green-500 to-green-500 lg:inline">TOTC
+                        </span> 
+                    </h2>
+                    <p className="mb-20 text-center mt-4 text-lg text-gray-900">TOTC is a platform that allows educators to create online classes whereby they can store the course materials online; manage assignments, quizzes and exams; monitor due dates; grade results and provide students with feedback all in one place.
+                    </p>
+                    <div className="grid grid-cols-1 gap-6   lg:grid-cols-4 ">
+                        <a href="#" className=" shadow-2xl relative ">
+                            <div className="  h-full relative shadow-2xl shadow-green-900 overflow-hidden group ">
+                                <div className=" absolute -bottom-10 group-hover:top-0 left-0 w-full h-full group-hover:bg-green-900 transition-all ease-in-out duration-500  ">
+                                    <div className="w-full h-full   p-5   relative">
+                                        <div className="absolute bottom-0 group-hover:bottom-24 text-white  text-left   transition-all ease-in-out duration-500 ">
+                                            <h2 className="text-2xl font-bold  text-white mb-0 pb-1">Standard Color</h2>
+                                            <p className="text-lg font-light text-white">Lorem ipsum dolor sit amet, #brands.</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <img src="https://source.unsplash.com/random/400x400" className="w-full z-0  h-full    object-fill example " />
+                            </div>
+                        </a>
+                        <a href="#" className=" shadow-2xl relative ">
+                            <div className="  h-full relative shadow-2xl shadow-green-900 overflow-hidden group ">
+                                <div className=" absolute -bottom-10 group-hover:top-0 left-0 w-full h-full group-hover:bg-green-900 transition-all ease-in-out duration-500  ">
+                                    <div className="w-full h-full   p-5   relative">
+                                        <div className="absolute bottom-0 group-hover:bottom-24 text-white  text-left   transition-all ease-in-out duration-500 ">
+                                            <h2 className="text-2xl font-bold  text-white mb-0 pb-1">Standard Color</h2>
+                                            <p className="text-lg font-light text-white">Lorem ipsum dolor sit amet, #brands.</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <img src="https://source.unsplash.com/random/400x400" className="w-full z-0  h-full    object-fill example " />
+                            </div>
+                        </a>
+                        <a href="#" className=" shadow-2xl relative ">
+                            <div className="  h-full relative shadow-2xl shadow-green-900 overflow-hidden group ">
+                                <div className=" absolute -bottom-10 group-hover:top-0 left-0 w-full h-full group-hover:bg-green-900 transition-all ease-in-out duration-500  ">
+                                    <div className="w-full h-full   p-5   relative">
+                                        <div className="absolute bottom-0 group-hover:bottom-24 text-white  text-left   transition-all ease-in-out duration-500 ">
+                                            <h2 className="text-2xl font-bold  text-white mb-0 pb-1">Standard Color</h2>
+                                            <p className="text-lg font-light text-white">Lorem ipsum dolor sit amet, #brands.</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <img src="https://source.unsplash.com/random/400x400" className="w-full z-0  h-full    object-fill example " />
+                            </div>
+                        </a>
+                        <a href="#" className=" shadow-2xl relative ">
+                            <div className="  h-full relative shadow-2xl shadow-green-900 overflow-hidden group ">
+                                <div className=" absolute -bottom-10 group-hover:top-0 left-0 w-full h-full group-hover:bg-green-900 transition-all ease-in-out duration-500  ">
+                                    <div className="w-full h-full   p-5   relative">
+                                        <div className="absolute bottom-0 group-hover:bottom-24 text-white  text-left   transition-all ease-in-out duration-500 ">
+                                            <h2 className="text-2xl font-bold  text-white mb-0 pb-1">Standard Color</h2>
+                                            <p className="text-lg font-light text-white">Lorem ipsum dolor sit amet, #brands.</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <img src="https://source.unsplash.com/random/400x400" className="w-full z-0  h-full    object-fill example " />
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </section>
+
+        </div>
+    )
+}
+
+export default TOTC
--- thecourse_user/src/pages/Home.jsx
+++ thecourse_user/src/pages/Home.jsx
@@ -3,12 +3,14 @@ import React from 'react'
 import Header from '../component/Header'
 import OurSuccess from '../component/OurSuccess'
 import CloudSoftWare from '../component/CloudSoftWare'
+import TOTC from '../component/TOTC'
 function Home() {
     return (
         <div>
             <Header/>
             <OurSuccess/>
             <CloudSoftWare/>
+            <TOTC/>
         </div>
     )
 }
