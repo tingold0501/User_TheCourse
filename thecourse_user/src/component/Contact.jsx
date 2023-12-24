6350ada691ce3856b7120d0da868e9976411ec43
Phạm Huỳnh Tín <65479150+tingold0501@users.noreply.github.com>
2023-12-24 17:06:51 +0700

    .....

--- thecourse_user/src/component/Contact.jsx
+++ thecourse_user/src/component/Contact.jsx
@@ -1,16 +1,73 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
-
+import React, { useState } from 'react'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import axios from 'axios'
 function Contact() {
+    <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+    />
+    {/* Same as */ }
+    <ToastContainer />
+
+    const urlApi = 'http://127.0.0.1:8000/api/';
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
+
+    const submitMessage = () => {
+        if (email == '') {
+            toast.error('🦄 Email is null!', {
+                position: "top-right",
+                autoClose: 1000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "light",
+            });
+        }
+        else if(message == ''){
+            toast.error('🦄 Message is null!', {
+                position: "top-right",
+                autoClose: 1000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "light",
+            });
+        }
+        else{
+            axios({
+                method: 'post',
+                url: urlApi + 'sendResquest',
+                data: {
+                    email: email,
+                    message: message,
+                }
+            }).then((res)=>{
+                console.log(res.data);
+            })
+        }
+    }
     return (
         <div>
+            <ToastContainer/>
             {/* component */}
             <section className="text-gray-600 body-font relative">
                 <div className="absolute inset-0 bg-gray-300">
                     <iframe width="100%" height="100%" frameBorder={0} marginHeight={0} marginWidth={0} title="map" scrolling="no" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d251289.9826291269!2d103.79260962544593!3d10.22880397391525!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31a78c62b49eda17%3A0x8aa79fbbdd72cdb!2zUGjDuiBRdeG7kWM!5e0!3m2!1svi!2s!4v1703404574554!5m2!1svi!2s" style={{}} />
-                    {/* <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d251289.9826291269!2d103.79260962544593!3d10.22880397391525!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31a78c62b49eda17%3A0x8aa79fbbdd72cdb!2zUGjDuiBRdeG7kWM!5e0!3m2!1svi!2s!4v1703404574554!5m2!1svi!2s"  style="border:0;"></iframe> */}
-                
-                
                 </div>
                 <div className="container px-5 py-24 mx-auto flex">
                     <div className="lg:w-1/3 md:w-1/2 bg-white rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0 relative z-10 shadow-md">
@@ -18,18 +75,16 @@ function Contact() {
                         <p className="leading-relaxed mb-5 text-gray-600">Plase Contact Us</p>
                         <div className="relative mb-4">
                             <label htmlFor="email" className="leading-7 text-sm text-gray-600">Email</label>
-                            <input type="email" id="email" name="email" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+                            <input onChange={e=>setEmail(e.target.value)} type="email" id="email" name="email" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
                         </div>
                         <div className="relative mb-4">
                             <label htmlFor="message" className="leading-7 text-sm text-gray-600">Message</label>
-                            <textarea id="message" name="message" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out" defaultValue={""} />
+                            <textarea onChange={e=>setMessage(e.target.value)} id="message" name="message" className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out" defaultValue={""} />
                         </div>
-                        <button className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
-                        <p className="text-xs text-gray-500 mt-3">Chicharrones blog helvetica normcore iceland tousled brook viral artisan.</p>
+                        <button onClick={submitMessage} className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Submit</button>
                     </div>
                 </div>
             </section>
-
         </div>
     )
 }
